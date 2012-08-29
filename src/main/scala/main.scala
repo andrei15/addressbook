ce8fd7dfc6ac49f32e3117f9218f53c0c2e8b16a
Andrey Semenow <semenow.andrew@gmail.com>
2012-08-29 10:02:30 +0400

    initial

---
+++ src/main/scala/main.scala
@@ -0,0 +1,14 @@
+package net.whiteants
+
+import ru.circumflex._, core._, web._, freemarker._
+import java.util.Date
+
+class Main extends Router {
+  val log = new Logger("net.whiteants")
+
+  'currentDate := new Date
+
+  get("/test") = "I'm fine, thanks!"
+  get("/") = ftl("index.ftl")
+
+}
